83f4bc011f05b5f2ca7907ad7396a3e933bc73f5
Saneesh Saji <65639829+ssaji1@users.noreply.github.com>
2021-06-05 22:36:31 -0400

    adding complete solutions

---
+++ IsItALeapYear/IsItALeapYear/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsItALeapYear
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+        public Boolean IsItALeapYear(int year)
+        {
+            if(year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
